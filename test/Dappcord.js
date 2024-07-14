4d3d842837ec548770cc9f6ecffe4c2f018fb007
M.Mahdi Saeidi <M.Mahdisaeidi99@gmail.com>
2024-07-14 11:23:59 +0330

    feat(Dappcord): add constructor and write tests for it

--- test/Dappcord.js
+++ test/Dappcord.js
@@ -5,5 +5,32 @@ const tokens = (n) => {
 }
 
 describe("Dappcord", function () {
+  let dappcord, deployer, user;
 
+  const NAME = "Dappcord";
+  const SYMBOL = "DC";
+
+  beforeEach(async () => {
+    [deployer, user] = await ethers.getSigners();
+    // Deploy Contract
+    const Dappcord = await ethers.getContractFactory("Dappcord");
+    dappcord = await Dappcord.deploy(NAME, SYMBOL);
+  })
+
+  describe('Deployment', () => {
+    it("Sets the name", async () => {
+      let result = await dappcord.name();
+      expect(result).to.equal(NAME);
+    })
+
+    it("Sets the symbol", async () => {
+      result = await dappcord.symbol();
+      expect(result).to.equal(SYMBOL);
+    })
+
+    it("Sets the owner", async () => {
+      const result = await dappcord.owner();
+      expect(result).to.equal(deployer.address);
+    })
+  })
 })
