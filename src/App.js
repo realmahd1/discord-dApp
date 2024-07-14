b9facddaf01f622db41b3e331f37fd0dfb2653fe
M.Mahdi Saeidi <M.Mahdisaeidi99@gmail.com>
2024-07-14 19:32:31 +0330

    feat(navbar): add navigation component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
-import { useEffect, useState } from 'react'
 import { ethers } from 'ethers'
+import { useEffect, useState } from 'react'
 import { io } from "socket.io-client"
 
 // Components
 import Navigation from './components/Navigation'
-import Servers from './components/Servers'
-import Channels from './components/Channels'
-import Messages from './components/Messages'
 
 // ABIs
 import Dappcord from './abis/Dappcord.json'
 
 // Config
-import config from './config.json';
+import config from './config.json'
 
 // Socket
 const socket = io('ws://localhost:3030');
 
 function App() {
+  const [account, setAccount] = useState(null);
+  const [provider, setProvider] = useState(null)
+  const [dappcord, setDappcord] = useState(null)
+
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider)
+
+    const network = await provider.getNetwork()
+    const dappcord = new ethers.Contract(config[network.chainId].Dappcord.address, Dappcord.abi, provider)
+    setDappcord(dappcord)
+
+    window.ethereum.on('accountsChanged', async () => {
+      window.location.reload()
+    })
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, [])
 
   return (
     <div>
-      <h1 style={{ textAlign: "center", padding: "15px" }}>Welcome to Dappcord</h1>
+      <Navigation account={account} setAccount={setAccount} />
 
       <main>
 
